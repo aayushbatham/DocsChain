b66bb275a699ae214adfbc6249c44eb830cb092a
Aayush <aayushbatham210036@acropolis.in>
2024-04-13 15:43:49 +0530

    orcus ke liye

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+const env = await import.meta.env;
+
+const clientId = env.clientId;
+console.log(clientId);
 
 const activeChainId = ChainId.Mumbai;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ThirdwebProvider desiredChainId={activeChainId}>
+  <ThirdwebProvider desiredChainId={activeChainId} clientId={clientId}>
     <App />
   </ThirdwebProvider>,
 )
