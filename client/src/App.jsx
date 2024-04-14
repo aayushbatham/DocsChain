dbc2f039400db75d4395ab48cb47708cfb42a5b8
Aayush <aayushbatham210036@acropolis.in>
2024-04-14 13:29:42 +0530

    frontend updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import Upload from './services/ipfsService';
+import Homepage from './pages/Homepage';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-const App = () => {
-  return (
-    <div>
-      <Upload />
-    </div>
-  )
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
-export default App
+
+// const App = () => {
+//   return (
+//     <>
+//       <Homepage/>
+//     </>
+//   )
+// }
+
+
+
+export default router
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 const clientId = '7b11d6a95c83dc53431667ef5799f0ca';
 console.log(clientId);
@@ -10,7 +14,9 @@ console.log(clientId);
 const activeChainId = ChainId.Mumbai;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ThirdwebProvider desiredChainId={activeChainId} clientId={clientId}>
-    <App />
-  </ThirdwebProvider>,
+  <React.StrictMode>
+      <ThirdwebProvider desiredChainId={activeChainId} clientId={clientId}>
+        <RouterProvider router={App} />
+      </ThirdwebProvider>,
+  </React.StrictMode>
 )
